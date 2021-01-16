9eb42b530492eca512d947d5009e4e9822f3ab0b
emekoba <klojett.re@gmail.com>
2021-01-16 01:48:49 +0100

    massively thinned down the excess code favour of simplicity on this branch based on this requirement...so no routers used, no links, no excess pages, etc..

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 import { useEffect } from "react";
-import { Route, BrowserRouter as Router } from "react-router-dom";
 import { useTimer } from "react-timer-hook";
 import "./App.css";
 import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
-import About from "./Routes/About/About";
-import Home from "./Routes/Home/Home";
-import ServiceTimes from "./Routes/ServiceTimes/ServiceTimes";
-import Login from "./Routes/Login/Login";
-import SignUp from "./Routes/SignUp/SignUp";
-import Connect from "./Routes/Connect/Connect";
-import Give from "./Routes/Give/Give";
-import ShoppingCart from "./Routes/ShoppingCart/ShoppingCart";
-import Events from "./Routes/Events/Events";
-import Sermons from "./Routes/Sermons/Sermons";
-import Store from "./Routes/Store/Store";
+import Home from "./Pages/Home";
 
 function App() {
 	const time = new Date();
@@ -38,33 +27,11 @@ function App() {
 
 	return (
 		<div className="App">
-			<Router>
-				<Header />
-				<Route
-					path="/"
-					exact
-					component={Home}
-					render={() => (
-						<Home
-							seconds={seconds}
-							minutes={minutes}
-							hours={hours}
-							days={days}
-						/>
-					)}
-				/>
-				<Route path="/aboutPage" exact component={About} />
-				<Route path="/serviceTimes" exact component={ServiceTimes} />
-				<Route path="/eventsPage" exact component={Events} />
-				<Route path="/store" exact component={Store} />
-				<Route path="/give" exact component={Give} />
-				<Route path="/shoppingCart" exact component={ShoppingCart} />
-				<Route path="/sermons" exact component={Sermons} />
-				<Route path="/connect" exact component={Connect} />
-				<Route path="/signUp" exact component={SignUp} />
-				<Route path="/login" exact component={Login} />
-				<Footer />
-			</Router>
+			<Header />
+
+			<Home />
+
+			<Footer />
 		</div>
 	);
 }
